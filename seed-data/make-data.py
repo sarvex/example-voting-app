7f39ef42d138474debe9018ae854cd4d50fb5555
Sourcery AI <>
2023-05-12 06:44:10 +0000

    'Refactored by Sourcery'

--- seed-data/make-data.py
+++ seed-data/make-data.py
@@ -1,13 +1,11 @@
 # this creates urlencode-friendly files without EOL
 import urllib.parse
 
-outfile = open('postb', 'w')
-params = ({ 'vote': 'b' })
-encoded = urllib.parse.urlencode(params)
-outfile.write(encoded)
-outfile.close()
-outfile = open('posta', 'w')
-params = ({ 'vote': 'a' })
-encoded = urllib.parse.urlencode(params)
-outfile.write(encoded)
-outfile.close()
+with open('postb', 'w') as outfile:
+    params = ({ 'vote': 'b' })
+    encoded = urllib.parse.urlencode(params)
+    outfile.write(encoded)
+with open('posta', 'w') as outfile:
+    params = ({ 'vote': 'a' })
+    encoded = urllib.parse.urlencode(params)
+    outfile.write(encoded)
